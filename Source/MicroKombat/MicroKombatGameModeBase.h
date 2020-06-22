a441e95a701bb17ee0bdf4028bc5eb0fa10299eb
Tri M. Cao <trimcao@gmail.com>
2020-06-21 22:49:55 -0700

    first commit

---
+++ Source/MicroKombat/MicroKombatGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MicroKombatGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MICROKOMBAT_API AMicroKombatGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
