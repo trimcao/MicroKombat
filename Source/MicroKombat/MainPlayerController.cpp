4314120da3ecb7cd15aee763c8617491f5d57c54
Tri M. Cao <trimcao@gmail.com>
2020-07-11 23:37:55 -0700

    start working on HUD (to show health, stamina, etc.)

---
+++ Source/MicroKombat/MainPlayerController.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MainPlayerController.h"
+#include "Blueprint/UserWidget.h"
+
+void AMainPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HUDOverlayAsset)
+	{
+		HUDOverlay = CreateWidget<UUserWidget>(this, HUDOverlayAsset);
+	}
+
+	if (HUDOverlay)
+	{
+		HUDOverlay->AddToViewport();
+		HUDOverlay->SetVisibility(ESlateVisibility::Visible);
+	}
+
+}
